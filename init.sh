a40a9cd9ad2207124c325e4d2c3402638e764896
mochi8k <y.hkh.185@gmail.com>
2017-10-17 10:35:02 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -6,9 +6,19 @@ apt-get install git make cmake zlib1g-dev g++
 
 git clone https://github.com/h2o/h2o.git
 cd h2o
+openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout server.key -out server.crt -subj "/CN=example.com" -days 3650
 git submodule update --init --recursive
 cmake .
 make
 make install
 
 echo `h2o --version`
+
+curl -L git.io/nodebrew | perl - setup
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+
+echo `nodebrew --version`
+
+nodebrew install-binary latest
+
+echo `nodebrew ls`
