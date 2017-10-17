df1c5ee463a291bfa49f59a1cd57335222350112
mochi8k <y.hkh.185@gmail.com>
2017-10-17 11:10:50 +0900

    connect h2o and node

--- init.sh
+++ init.sh
@@ -4,8 +4,10 @@ set -e
 
 apt-get install git make cmake zlib1g-dev g++
 
+# for h2o
+
 git clone https://github.com/h2o/h2o.git
-cd h2o
+cd /root/work/h2o
 openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout server.key -out server.crt -subj "/CN=example.com" -days 3650
 git submodule update --init --recursive
 cmake .
@@ -14,11 +16,19 @@ make install
 
 echo `h2o --version`
 
+
+# for node
+
 curl -L git.io/nodebrew | perl - setup
-export PATH=$HOME/.nodebrew/current/bin:$PATH
+
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> /root/.bashrc
 
 echo `nodebrew --version`
 
 nodebrew install-binary latest
+nodebrew use latest
 
 echo `nodebrew ls`
+
+cd /root/work/server
+npm install
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
-const options = {
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.crt')
-};
+const express = require('express');
+const app = express();
 
-require('http2').createServer(options, function(request, response) {
-    response.end('Hello world!');
-}).listen(3000);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
